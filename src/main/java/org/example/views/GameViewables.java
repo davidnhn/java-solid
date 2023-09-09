76c7ecb56bd478334991368f40930cdf45b16104
David Nhn <david.nahon@hotmail.fr>
2023-09-09 17:30:35 +0200

    design pattern composite

---
+++ src/main/java/org/example/views/GameViewables.java
@@ -0,0 +1,74 @@
+package org.example.views;
+
+import org.example.controller.GameController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameViewables implements GameViewable {
+
+    List<GameViewable> views;
+
+    public GameViewables () {
+        views = new ArrayList<GameViewable>();
+    }
+
+    public void addViewable (GameViewable view) {
+        views.add(view);
+    }
+    @Override
+    public void setController(GameController gc) {
+    for (GameViewable view : views) {
+        view.setController(gc);
+    }
+    }
+
+    @Override
+    public void promptForPlayerName() {
+        for (GameViewable view : views) {
+            view.promptForPlayerName();
+        }
+    }
+
+    @Override
+    public void showPlayerName(int playerIndex, String playerName) {
+        for (GameViewable view : views) {
+            view.showPlayerName(playerIndex, playerName);
+        }
+    }
+
+    @Override
+    public void showFaceDownCardForPlayer(int playerIndex, String playerName) {
+        for (GameViewable view : views) {
+            view.showPlayerName(playerIndex, playerName);
+        }
+    }
+
+    @Override
+    public void promptForFlip() {
+        for (GameViewable view : views) {
+            view.promptForFlip();
+        }
+    }
+
+    @Override
+    public void showCardForPlayer(int playerIndex, String playerName, String rank, String suit) {
+        for (GameViewable view : views) {
+            view.showCardForPlayer(playerIndex, playerName, rank, suit);
+        }
+    }
+
+    @Override
+    public void showWinner(String winnerName) {
+        for (GameViewable view : views) {
+            view.showWinner(winnerName);
+        }
+    }
+
+    @Override
+    public void promptForNewGame() {
+        for (GameViewable view : views) {
+            view.promptForNewGame();
+        }
+    }
+}
